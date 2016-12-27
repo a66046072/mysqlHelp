cfc637a9964cec0471d594696715e3e6452e7e0e
a66046072 <709795385@qq.com>
2016-12-27 11:22:05 +0800

    MySqlHelper

---
+++ MySqlHelperExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication2
+{
+    /// <summary>
+    /// MySqlHelper扩展(依赖AutoMapper.dll)
+    /// </summary>
+    public sealed partial class MySqlHelper
+    {
+        #region 使用默认连接字符串方法
+
+        public static T ExecuteObject<T>(string commandText, params MySqlParameter[] parms)
+        {
+            return ExecuteObject<T>(ConnectionString, commandText, parms);
+        }
+
+        public static List<T> ExecuteObjects<T>(string commandText, params MySqlParameter[] parms)
+        {
+            return ExecuteObjects<T>(ConnectionString, commandText, parms);
+        }
+
+        #endregion
+
+        #region 手动传入连接字符串方法
+
+        public static T ExecuteObject<T>(string connectionString, string commandText, params MySqlParameter[] parms)
+        {
+            //DataTable dt = ExecuteDataTable(connectionString, commandText, parms);
+            //return AutoMapper.Mapper.DynamicMap<List<T>>(dt.CreateDataReader()).FirstOrDefault();
+            using (MySqlDataReader reader = ExecuteDataReader(connectionString, commandText, parms))
+            {
+                return AutoMapper.Mapper.DynamicMap<List<T>>(reader).FirstOrDefault();
+            }
+        }
+
+        public static List<T> ExecuteObjects<T>(string connectionString, string commandText, params MySqlParameter[] parms)
+        {
+            //DataTable dt = ExecuteDataTable(connectionString, commandText, parms);
+            //return AutoMapper.Mapper.DynamicMap<List<T>>(dt.CreateDataReader());
+            using (MySqlDataReader reader = ExecuteDataReader(connectionString, commandText, parms))
+            {
+                return AutoMapper.Mapper.DynamicMap<List<T>>(reader);
+            }
+        }
+
+        #endregion
+    }
+
+
+}
